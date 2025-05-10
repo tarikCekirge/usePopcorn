5ac9493bc69c8942f9da8daf3e3bf2a2948ab956
Tarık Çekirge <tcekirge@gmail.com>
2025-05-10 19:13:30 +0300

    Refs to Select DOM Elements

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,16 +1,40 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useDebounce } from 'use-debounce';
 
 const Search = ({ query, setQuery }) => {
     const [input, setInput] = useState(query);
     const [debouncedInput] = useDebounce(input, 500);
+    const inputEl = useRef(null);
 
     useEffect(() => {
         setQuery(debouncedInput);
     }, [debouncedInput, setQuery]);
 
+    useEffect(() => {
+        const handleEnterDown = (e) => {
+
+            if (document.activeElement === inputEl.current) return
+            if (e.code === "Enter" && inputEl.current) {
+                inputEl.current.focus();
+                setInput("");
+                setQuery("");
+            }
+        };
+
+        document.addEventListener("keydown", handleEnterDown);
+
+        if (inputEl.current) {
+            inputEl.current.focus();
+        }
+
+        return () => {
+            document.removeEventListener("keydown", handleEnterDown);
+        };
+    }, [setQuery]);
+
     return (
         <input
+            ref={inputEl}
             className="search"
             type="text"
             placeholder="Search movies..."
